341804cd7547bace3cfc8f76007566c5150c702b
I-OutOfGravity-I <108941631+I-OutOfGravity-I@users.noreply.github.com>
2023-12-10 00:35:14 +0100

    nice

--- ChatAppServerMaxS/Program.cs
+++ ChatAppServerMaxS/Program.cs
@@ -1,14 +1,10 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 
 public enum MessageType
 {
+    Login,
     ServerNotification,
     Message,
     ConnectedUsers,
@@ -16,51 +12,72 @@ public enum MessageType
     Video
 }
 
+[Serializable]
 public class Message
 {
     public MessageType Type { get; set; }
-    public byte[] Content { get; set; }
+    public string Username { get; set; }
+    public string Content { get; set; }
 
-    public Message (MessageType type, string content)
+    [System.Text.Json.Serialization.JsonConstructor]
+    public Message(MessageType type, string username, string content)
     {
         Type = type;
-        Content = Encoding.UTF8.GetBytes (content);
-    }
-    public Message(MessageType type, List<string> content)
-    {
-        Type = type;    
-        Content = ConvertListToByteArray(content);
-    }
-    public Message(MessageType type, byte[] content)
-    {
-        Type = type;
+        Username = username;
         Content = content;
     }
-    public string Serialize()
+
+    public static string SerializeMessage(Message message)
     {
-        return $"{(int)Type}#{Convert.ToBase64String(Content)}";
+        string jsonString = System.Text.Json.JsonSerializer.Serialize(message);
+        return jsonString;
     }
-    static byte[] ConvertListToByteArray(List<string> stringList)
+
+    public static Message DeserializeMessage(string jsonString)
     {
-        // Use UTF-8 encoding to convert each string to bytes
-        List<byte[]> stringBytesList = new List<byte[]>();
-        foreach (string str in stringList)
-        {
-            byte[] strBytes = Encoding.UTF8.GetBytes(str);
-            stringBytesList.Add(strBytes);
-        }
+        Message deserializedMessage = System.Text.Json.JsonSerializer.Deserialize<Message>(jsonString);
+        return deserializedMessage;
+    }
 
-        // Concatenate the byte arrays into a single byte array
-        int totalLength = stringBytesList.Sum(arr => arr.Length);
-        byte[] result = new byte[totalLength];
-        int offset = 0;
-        foreach (byte[] strBytes in stringBytesList)
-        {
-            Buffer.BlockCopy(strBytes, 0, result, offset, strBytes.Length);
-            offset += strBytes.Length;
-        }
+    public static Message CreateMessage(MessageType type, string username, string content)
+    {
+        return new Message(type, username, content);
+    }
 
-        return result;
+    public static Message CreateServerMessage(MessageType type, string username, List<string> content)
+    {
+        return new Message(type, "", SerializeStringList(content));
+    }
+    public static Message CreateServerMessage(MessageType type, string username, string content)
+    {
+        return new Message(type, "", content);
+    }
+    public static string SerializeStringList(List<string> stringList)
+    {
+        // Manually create a JSON-formatted string for the list
+        string jsonString = "[";
+        for (int i = 0; i < stringList.Count; i++)
+        {
+            jsonString += $"\"{EscapeString(stringList[i])}\"";
+
+            if (i < stringList.Count - 1)
+            {
+                jsonString += ",";  
+            }
+        }
+        jsonString += "]";
+
+        return jsonString;
+    }
+
+    public static string EscapeString(string input)
+    {
+        return input.Replace("\"", "\\\"");
+    }
+
+    public static string UnescapeString(string input)
+    {
+        return input.Replace("\\\"", "\"");
     }
 }
 
@@ -77,7 +94,7 @@ public class Client
 
     public string Username { get => username; set => username = value; }
 
-    public ClientHandler ClientHandler => clientHandler;
+    internal ClientHandler GetClientHandler() => clientHandler;
 }
 
 public class ChatServer
@@ -91,15 +108,37 @@ public class ChatServer
         listener = new TcpListener(ipAddress, port);
     }
 
+    private void SendConnectedUser()
+    {
+        while (true)
+        {
+            Thread.Sleep(1000);
+            if (clients.Count > 0)
+            {
+                Thread.Sleep(200);
+                List<string> usernameAndAddress = new List<string>();
+
+                foreach (var client in clients)
+                {
+                    usernameAndAddress.Add(client.Username);
+                }
+                BroadcastMessage(Message.CreateServerMessage(MessageType.ConnectedUsers, "", usernameAndAddress));
+            }
+        }
+    }
+
     public void Start()
     {
         listener.Start();
-        Console.WriteLine("Server started. Waiting for clients...");
+        Log("Server started. Waiting for clients...");
+
+        Thread sendUserThread = new Thread(() => SendConnectedUser());
+        sendUserThread.Start();
 
         while (true)
         {
             TcpClient tcpClient = listener.AcceptTcpClient();
-            Console.WriteLine("Client connected.");
+            Log("Client connected.");
 
             ClientHandler clientHandler = new ClientHandler(tcpClient, this);
 
@@ -113,23 +152,28 @@ public class ChatServer
         }
     }
 
-    public void BroadcastMessage(Message message, ClientHandler sender)
+    internal void BroadcastMessage(Message message)
     {
         lock (clientsLock)
         {
             foreach (var client in clients)
             {
-                client.ClientHandler.SendMessage(message);
+                client.GetClientHandler().SendMessage(message);
             }
         }
     }
 
-    public void RemoveClient(Client client)
+    internal void RemoveClient(Client client)
     {
         lock (clientsLock)
         {
             clients.Remove(client);
         }
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}");
     }
 }
 
@@ -154,49 +198,67 @@ public class ClientHandler
     {
         try
         {
-            client.Username = reader.ReadLine();
-            Console.WriteLine($"{client.Username} joined the chat. {client.ClientHandler.client.Client.RemoteEndPoint}");
-            server.BroadcastMessage(new Message(MessageType.ServerNotification, $"{client.Username} joined the chat." ), this);
+            string registerData = reader.ReadLine();
+            Message registerMessage = Message.DeserializeMessage(registerData);
+            client.Username = registerMessage.Username;
+            server.Log($"{registerMessage.Username} joined the chat. {client.GetClientHandler().client.Client.RemoteEndPoint}");
+            server.BroadcastMessage(Message.CreateServerMessage(MessageType.ServerNotification, "", $"{registerMessage.Username} joined the chat."));
 
             while (true)
             {
-                string message = reader.ReadLine();
-                if (message == null)
+
+                string stringData = reader.ReadLine();
+                if (stringData == null)
                     break;
-                Message messagee = new Message(MessageType.Message, $"{client.Username}: {message}");
-                Console.WriteLine(message);
-                Console.WriteLine(messagee.Type);
-                Console.WriteLine(messagee.Content);
-                server.BroadcastMessage(messagee, this);
+                Message message = Message.DeserializeMessage(stringData);
+                //hier kann man dann mitlauschen höhö
+                Message sendBackMessage = Message.CreateMessage(message.Type, message.Username, message.Content);
+                server.BroadcastMessage(sendBackMessage);
             }
+        }
+        catch (IOException ex)
+        {
+            server.Log($"{client.Username} disconnected: {ex.Message}");
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex.Message);
+            server.Log($"An error occurred in HandleClient: {ex.Message}");
         }
         finally
         {
+            try
+            {
+                server.RemoveClient(client);
+                reader.Close();
+                writer.Close();
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                server.Log($"Error while closing client resources: {ex.Message}");
+            }
+
             string tempUsername = client.Username;
-            server.RemoveClient(client);
-
-            // Properly close network resources
-            reader.Close();
-            writer.Close();
-            stream.Close();
-
-            server.BroadcastMessage(new Message(MessageType.ServerNotification, $"{tempUsername} disconnected."), this);
-            Console.WriteLine("Client disconnected.");
+            server.BroadcastMessage(Message.CreateMessage(MessageType.ServerNotification, "", $"{tempUsername} disconnected."));
+            server.Log("Client disconnected.");
         }
     }
 
-    public void SendMessage(Message message)
+    internal void SendMessage(Message message)
     {
-        string serializedMessage = JsonConvert.SerializeObject(message);
-        writer.WriteLine(serializedMessage);
-        writer.Flush();
-
-        //writer.WriteLine($"{message.Type}#{message.Content}");
-        //writer.Flush();
+        try
+        {
+            if (client.Connected)
+            {
+                string sendData = Message.SerializeMessage(message);
+                writer.WriteLine(sendData);
+                writer.Flush();
+            }
+        }
+        catch (IOException ex)
+        {
+            server.Log($"Error sending message: {ex.Message}");
+        }
     }
 }
 
